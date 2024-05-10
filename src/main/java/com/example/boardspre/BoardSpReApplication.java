cf4f7475fe59700e5e3395b9dbc91c0e1d1c9796
dhy25n <sai4875@naver.com>
2024-05-10 21:02:29 +0900

    start project

---
+++ src/main/java/com/example/boardspre/BoardSpReApplication.java
@@ -0,0 +1,13 @@
+package com.example.boardspre;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoardSpReApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BoardSpReApplication.class, args);
+    }
+
+}
